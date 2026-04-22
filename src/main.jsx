5bdf13d999a580290cf9522f05f9ebac0a4a14a6
Mohit <s.mohit3547@gmail.com>
2026-04-22 12:19:57 +0530

    main file updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import './css/landingPage.css'
 import './css/designPage.css'
 import './css/projectpage.css'
 import './css/introduction.css'
-import './css/Footer.css'
+import './css/footer.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
